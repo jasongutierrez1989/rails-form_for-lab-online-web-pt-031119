824dcb9d9a3b2490bcb8a42973c366e40162402e
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2019-08-29 01:31:19 +0000

    Automatically backed up by Learn

--- app/controllers/student_controller.rb
+++ app/controllers/student_controller.rb
@@ -1,12 +1,35 @@
 class StudentController < ApplicationController
-  def new
+  def index
+    @students = Student.all
   end
-  def create
-  end
+
   def show
+    @student = Student.find(params[:id])
   end
-  def edit
+
+  def new
+    @student = Student.new
   end
+
+  def create
+    @student = Student.new(student_params(:first_name, :last_name))
+    @student.save
+    redirect_to student_path(@student)
+  end
+
   def update
+    @student = Student.find(params[:id])
+	  @student.update(student_params(:first_name, :last_name))
+	  redirect_to student_path(@student)
+  end
+
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+  private
+
+  def student_params(*args)
+    params.require(:student).permit(*args)
   end
 end
