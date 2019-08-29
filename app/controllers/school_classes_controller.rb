4df8b2d4947f9df0c449050d5c945862347ba3a1
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2019-08-29 01:22:48 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -6,11 +6,11 @@ class SchoolClassController < ActionController::Base
   def show
     @schoolclass = SchoolClass.find(params[:id])
   end
-  
+
   def new
     @schoolclass = SchoolClass.new
   end
-  
+
   def create
     @schoolclass = SchoolClass.new(schoolclass_params(:title, :room_number))
     @schoolclass.save
@@ -22,7 +22,7 @@ class SchoolClassController < ActionController::Base
 	  @schoolclass.update(post_params(:title))
 	  redirect_to post_path(@post)
   end
-  
+
   def edit
     @schoolclass = SchoolClass.find(params[:id])
   end
