fc0b1503823f8b9a3fc836523dccd8c1fa368f4d
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2019-08-29 02:13:43 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,35 +1,35 @@
-class SchoolClassController < ApplicationController
+class SchoolClassesController < ApplicationController
   def index
-    @schoolclasses = SchoolClass.all
+    @school_classes = SchoolClass.all
   end
 
   def show
-    @schoolclass = SchoolClass.find(params[:id])
+    @school_class = SchoolClass.find(params[:id])
   end
 
   def new
-    @schoolclass = SchoolClass.new
+    @school_class = SchoolClass.new
   end
 
   def create
-    @schoolclass = SchoolClass.new(schoolclass_params(:title, :room_number))
-    @schoolclass.save
-    redirect_to school_classes_path(@schoolclass)
+    @school_class = SchoolClass.new(school_class_params(:title, :room_number))
+    @school_class.save
+    redirect_to school_class_path(@school_class)
   end
 
   def update
-    @schoolclass = SchoolClass.find(params[:id])
-	  @schoolclass.update(post_params(:title))
-	  redirect_to school_classes_path(@schoolclass)
+    @school_class = SchoolClass.find(params[:id])
+	  @school_class.update(school_class_params(:title, :room_number))
+	  redirect_to school_classes_path(@school_class)
   end
 
   def edit
-    @schoolclass = SchoolClass.find(params[:id])
+    @school_class = SchoolClass.find(params[:id])
   end
 
   private
 
-  def schoolclass_params(*args)
-    params.require(:school_classes).permit(*args)
+  def school_class_params(*args)
+    params.require(:school_class).permit(*args)
   end
 end
