00ea9c51d8a7d94c3c45ac49cfdd09d0d2a2e0ba
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2019-08-29 01:15:36 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,12 +1,20 @@
 class SchoolClassController < ActionController::Base
+  def index
+    @schoolclasses = SchoolClass.all
+  end
   def new
+    @schoolclass = SchoolClass.new
   end
   def create
+    @schoolclass = SchoolClass.new(post_params())
   end
   def show
   end
   def edit
   end
   def update
+  end
+  def schoolclass_params(*args)
+    params.require(:schoolclass).permit(*args)
   end
 end
